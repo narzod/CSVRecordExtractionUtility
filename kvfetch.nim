df59264da2b8489e3c72aa2dbf2f2877b93ea8c6
narzod <jesinger@gmail.com>
2024-06-09 15:15:09 -0400

    feat: Add exit status codes to kvfetch.nim

--- kvfetch.nim
+++ kvfetch.nim
@@ -15,20 +15,33 @@
 import
   os, osproc, strutils
 
+const
+  EXIT_SUCCESS = 0
+  EXIT_INSUFFICIENT_ARGS = 1
+  EXIT_FAILURE_TO_RETRIEVE_SOURCE = 2
+  EXIT_RETRIEVED_SOURCE_EMPTY = 3
+  EXIT_NO_MATCH_FOR_RECORD = 4
+
 proc isURL(input: string): bool =
   return input.find("://") != -1
 
 proc getFileContent(fileLocator: string): string =
   if fileLocator.endswith(":"):
-    return ""
+    quit(EXIT_FAILURE_TO_RETRIEVE_SOURCE)
 
   let isFile = not isURL(fileLocator)
   let cmd = case isFile:
     of true: "rclone cat " & fileLocator
     of false: "rclone copyurl " & fileLocator & " --stdout"
 
-  var (content, _) = execCmdEx(cmd)
+  var (content, status) = execCmdEx(cmd)
   content = content.strip()
+
+  if status > 0:
+    quit(EXIT_FAILURE_TO_RETRIEVE_SOURCE)
+
+  if content.len == 0:
+    quit(EXIT_RETRIEVED_SOURCE_EMPTY)
 
   if isFile and isURL(content):
     return getFileContent(content)
@@ -39,6 +52,7 @@ proc getRowMatchingSelector(content: string, rowSelector: string): string =
   for line in content.splitLines()[1..^1]:
     if line.startswith(rowSelector & ","):
       return line
+  quit(EXIT_NO_MATCH_FOR_RECORD)
 
 proc printInPairs(header: seq[string], row: seq[string]): void =
   for i, headerElem in header.pairs:
@@ -52,7 +66,14 @@ if paramCount() < 2:
   echo "Where:"
   echo "  <record>    Matches the first column of the target row."
   echo "  <source>    Specifies URL, file path, or rclone remote."
-  echo "              Files may contain either CSV data or a URL."
+  echo "              Files may contain either CSV data or a URL.\n"
+  echo "Exit Status Codes:"
+  echo "  0 - Success"
+  echo "  1 - Insufficient arguments"
+  echo "  2 - Failure to retrieve source"
+  echo "  3 - Retrieved source is empty"
+  echo "  4 - No match for specified record"
+  quit(EXIT_INSUFFICIENT_ARGS)
 else:
   let rowSelector = paramStr(1)
   let fileLocator = paramStr(2)
@@ -62,3 +83,5 @@ else:
 
   if selRowElems.len > 2:
     printInPairs(topRowElems, selRowElems)
+
+quit(EXIT_SUCCESS)
